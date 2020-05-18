a1546f41ffafdfad62b3324e6471fda47cf76767
root <root@localhost.localdomain>
2020-05-19 04:23:44 +0545

    api

--- app.py
+++ app.py
@@ -1,43 +1,72 @@
-import numpy as np
-import pandas as pd
-from flask import Flask, request, render_template
-import pickle
+"""This module will serve the api request."""
 
-# from models import load   (to load the model from models folder)
+from flask import Flask, Response, jsonify
+from flask_cors import CORS
+from flask_pymongo import PyMongo
+from services.user import create_user, fetch_users, fetch_user, update_user, remove_user
+from setting import shared_components
 
-app = Flask(__name__)  #
-# model=load.init()
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+def init_app():
 
-@app.route('/predict',methods=['POST'])
-def predict():
-    input_features = [float(x) for x in request.form.values()]
-    # features_value = [np.array(input_features)]
-    
-    # features_name = ['mean radius', 'mean texture', 'mean perimeter', 'mean area',
-    #    'mean smoothness', 'mean compactness', 'mean concavity',
-    #    'mean concave points', 'mean symmetry', 'mean fractal dimension',
-    #    'radius error', 'texture error', 'perimeter error', 'area error',
-    #    'smoothness error', 'compactness error', 'concavity error',
-    #    'concave points error', 'symmetry error', 'fractal dimension error',
-    #    'worst radius', 'worst texture', 'worst perimeter', 'worst area',
-    #    'worst smoothness', 'worst compactness', 'worst concavity',
-    #    'worst concave points', 'worst symmetry', 'worst fractal dimension']
-    
-    # df = pd.DataFrame(features_value, columns=features_name)
-    # # output = model.predict(df)
-        
-    # if output == 0:
-    #     res_val = "** breast cancer **"
-    # else:
-    #     res_val = "no breast cancer"
-        
+    app = Flask(__name__)
+    app = create_route(app)
 
-    return render_template('index.html', prediction_text=input_features)
+    # Load Config File for DB
+    app.config.from_pyfile('config/config.cfg')
+    CORS(app)
+    mongo = PyMongo(app)
 
-if __name__ == "__main__":
-    #app.run(debug=True)
-    app.run()    # for production level
+    # Select the database
+    db = mongo.db
+    shared_components['db'] = db
+    return app
+
+
+def create_route(app):
+    """
+    Adds different rules to the urls
+    """
+    app.add_url_rule(rule='/',
+                    view_func=get_initial_response, methods=['GET'])
+    app.add_url_rule(rule='/api/v1/users',
+                     view_func=create_user, methods=['POST'])
+    app.add_url_rule(rule='/api/v1/users',
+                     view_func=fetch_users, methods=['GET'])
+    app.add_url_rule(rule='/api/v1/users/<user_id>',
+                     view_func=fetch_user, methods=['GET'])
+    app.add_url_rule("/api/v1/users/<user_id>",
+                     view_func=update_user, methods=['PUT'])
+    app.add_url_rule("/api/v1/users/<user_id>",
+                     view_func=remove_user, methods=['DELETE'])
+    return app
+
+
+def get_initial_response():
+    """Welcome message for the API."""
+    # Message to the user
+    message = {
+        'api_version': 'v1.0',
+        'status': '200',
+        'message': 'Welcome to the Flask API'
+    }
+    # Making the message looks good
+    resp = jsonify(message)
+
+    # Returning the object
+    return resp
+
+
+app = init_app()
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    """Send message to the user with notFound 404 status."""
+    # Message to the user
+    message = "This route is currently not supported."
+    " Please refer API documentation."
+
+    # Sending OK response
+    # Returning the object
+    return Response(message, status=404)
